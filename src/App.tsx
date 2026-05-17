537fbde127acfa21264bf4803897e7fb0575c03c
Piyusha <piyushavpawar@gmail.com>
2026-05-17 22:48:55 +0530

    Logged all and files, learnings in readme

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count -1)}>
+        -
+      </button>
+
+      <button onClick={() => setCount(count +1)}>
+        +
+      </button>
+
+    </>
+  )
+}
+
+export default App
